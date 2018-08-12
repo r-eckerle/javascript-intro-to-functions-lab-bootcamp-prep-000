83e9813629b68a0801b19ec198f9b24605a84ea8
Ryan <eckerle_r@protonmail.com>
2018-08-12 02:07:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can't hear you!";
+  } else if (string === string.toLowerCase()) {
+    return "YES INDEED!";
   }
 }

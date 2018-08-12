47ba907d77098359d1d42740f5a4a7b8d582103d
Ryan <eckerle_r@protonmail.com>
2018-08-12 02:10:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
-  } else if (string === '')
+  } else if (string === 'I love you, Grandma.') {
+    return "I love you, too.";
+  } else {
+    
+  }
 }

9756faedf63bd9d00795b4712cf5da51936d0a58
Ryan <eckerle_r@protonmail.com>
2018-08-12 02:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if(string === string.toLowerCase) {
+    return "I can't hear you!";
+  }
+}
